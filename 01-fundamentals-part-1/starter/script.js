5dd393593bf8de8b8130efafa2b1bafaf78888c7
iameileenjulia <eileenjulia01@gmail.com>
2025-08-20 08:44:21 +0800

    feat: add basic variables and data types
    - Set up JavaScript environment and browser console
    - Implement let, const, var variable declarations
    - Add data type exploration with typeof operator
    - Establish development workflow and best practices

--- 01-fundamentals-part-1/starter/script.js
+++ 01-fundamentals-part-1/starter/script.js
@@ -1,2 +1,33 @@
 // JavaScript Fundamentals - Part 1
 // We'll write our code here!
+
+console.log("=== DATA TYPES ===");
+
+
+let age = 23;
+console.log(age);
+console.log(typeof age);
+
+
+let firstName = "Jonas";
+console.log(firstName);
+console.log(typeof firstName); 
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun); 
+
+
+let year;
+console.log(year); 
+console.log(typeof year); 
+
+let dynamicVariable = 23;
+console.log(dynamicVariable, typeof dynamicVariable);
+
+dynamicVariable = "Now I'm a string!";
+console.log(dynamicVariable, typeof dynamicVariable);
+
+dynamicVariable = true;
+console.log(dynamicVariable, typeof dynamicVariable);
